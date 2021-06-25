3327f4c55fc3d128e31b8d3636f344e348d20c94
unknown <imdtf@qq.com>
2021-06-25 23:11:53 +0800

    两个字典controller接口

--- MyAdmin-system/src/main/java/com/dtf/modules/system/rest/DictDetailController.java
+++ MyAdmin-system/src/main/java/com/dtf/modules/system/rest/DictDetailController.java
@@ -1,10 +1,24 @@
 package com.dtf.modules.system.rest;
 
 import com.dtf.modules.system.service.DictDetailService;
+import com.dtf.modules.system.service.dto.DictDetailDto;
+import com.dtf.modules.system.service.dto.DictDetailQueryCriteria;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 0 *
@@ -19,4 +33,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class DictDetailController {
     private final DictDetailService dictDetailService;
     private static final String ENTITY_NAME = "diceDetail";
+
+    @ApiOperation("查询字典详情")
+    @GetMapping
+    public ResponseEntity<Object> query(DictDetailQueryCriteria criteria, @PageableDefault(sort = {"dictSort"}, direction = Sort.Direction.ASC) Pageable pageable) {
+        return new ResponseEntity<>(dictDetailService.queryAll(criteria, pageable), HttpStatus.OK);
+    }
+
+    @ApiOperation("查询多个字典详情")
+    @GetMapping("/map")
+    public ResponseEntity<Object> getDictDetailMap(@RequestParam String dictName) {
+        String[] names = dictName.split("[,，]");
+        Map<String, List<DictDetailDto>> dictMap = new HashMap<>(16);
+        for (String name : names) {
+            dictMap.put(name, dictDetailService.getDictByName(name));
+        }
+        return new ResponseEntity<>(dictMap, HttpStatus.OK);
+    }
 }
